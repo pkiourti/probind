456ff873bb17dc05d494fc393a0ddaea1403fb89
Panayota Kiourti <pkiourti@gmail.com>
2020-04-02 05:19:48 -0400

    python script for creating and saving data

---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+from data import DNASeqGenerator
+
+
+def data_files():
+    i = 1
+    if os.path.exists(os.path.join('data', 'x_forward_' + str(i))):
+        i += 1
+
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    return (os.path.join('data', 'x_forward_' + str(i)),
+            os.path.join('data', 'x_reverse_' + str(i)),
+            os.path.join('data', 'y_' + str(i)))
+
+
+if __name__ == '__main__':
+    seq_generator = DNASeqGenerator(300)
+
+    seq = np.asarray([seq_generator.create_random_seq() for _ in range(100)])
+    forward = np.asarray(seq)[:, 0]
+    reverse = np.asarray(seq)[:, 1]
+    binding_value = [seq_generator.create_random_bind_value() for _ in range(100)]
+    forward_file, reverse_file, bind_v_file = data_files()
+    np.save(forward_file + '.npy', forward)
+    np.save(reverse_file + '.npy', reverse)
+    np.save(bind_v_file + '.npy', binding_value)
