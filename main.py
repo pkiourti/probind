bbd4673ecb584bdb3c9c4b09efe20655c80e8f3b
Penny Kiourti <pkiourti@gmail.com>
2020-04-03 17:12:48 -0400

    fix finding whether directory exists

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from generator import DNASeqGenerator
 
 def data_files():
     i = 1
-    if os.path.exists(os.path.join('data', 'x_forward_' + str(i))):
+    while os.path.exists(os.path.join('data', 'x_forward_' + str(i) + '.npy')):
         i += 1
 
     if not os.path.exists('data'):
@@ -19,10 +19,10 @@ def data_files():
 if __name__ == '__main__':
     seq_generator = DNASeqGenerator(300)
 
-    seq = np.asarray([seq_generator.create_random_seq() for _ in range(100)])
+    seq = np.asarray([seq_generator.create_random_seq() for _ in range(500)])
     forward = np.asarray(seq)[:, 0]
     reverse = np.asarray(seq)[:, 1]
-    binding_value = [seq_generator.create_random_bind_value() for _ in range(100)]
+    binding_value = [seq_generator.create_random_bind_value() for _ in range(500)]
     forward_file, reverse_file, bind_v_file = data_files()
     np.save(forward_file + '.npy', forward)
     np.save(reverse_file + '.npy', reverse)
