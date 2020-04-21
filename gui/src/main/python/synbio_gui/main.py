214e3ad6457c883d1cab4151393b6f9e96ff4161
tpham393 <thuyp@bu.edu>
2020-04-20 22:54:24 -0400

    update train_model gui

--- gui/src/main/python/synbio_gui/main.py
+++ gui/src/main/python/synbio_gui/main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
     window = QMainWindow()
     window.setCentralWidget(TrainModelWidget())
+    window.setWindowTitle("EC552 TF Orthogonality Tool")
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
