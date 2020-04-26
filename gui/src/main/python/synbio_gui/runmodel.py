d0ba18e71e9cca30180245339f92102a8253b98e
alyazyah <37453063+alyazyah@users.noreply.github.com>
2020-04-26 20:48:51 +0400

    Update runmodel.py

--- gui/src/main/python/synbio_gui/runmodel.py
+++ gui/src/main/python/synbio_gui/runmodel.py
@@ -1,11 +1,17 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import os
-from PyQt5.QtWidgets import (QLabel, QtSlider, QRadioButton, QPushButton, QComboBox, QVBoxLayout, QApplication, QWidget, QLineEdit, QMessageBox)
+from PyQt5.QtWidgets import (QLabel, QSlider, QRadioButton, QPushButton, QComboBox, QVBoxLayout, QApplication, QWidget, QLineEdit, QMessageBox)
 from PyQt5.QtCore import Qt
-from file_dialog import FileDialog
+from file_dialog import FileDialog, MultiFileDialog
 from utils import get_saved_models
-from cross_talk_evaluator import CrossTalkEvaluator
+from backend.cross_talk_evaluator import CrossTalkEvaluator
+
+project_root = os.environ.get('PYTHONPATH')
+try:
+    project_root = project_root.split(os.path.pathsep)[1]
+except Exception as e:
+    pass
 
 class RMW(QtWidgets.QWidget):
 
@@ -18,9 +24,9 @@ class RMW(QtWidgets.QWidget):
         layout.addWidget(rm_button)
         self.setLayout(layout)
 
-        path = '../../../../../models'
+        self.path = project_root
 
-        if os.path.exists(path):
+        if os.path.exists(os.path.join(self.path, 'models')):
             rm_button.clicked.connect(self.evalparam)
         else:
             rm_button.clicked.connect(self.nomodel)
@@ -49,11 +55,11 @@ class RMW(QtWidgets.QWidget):
         slider.setTickInterval(0.1)
      #   slider.valueChanged[int].connect(self.threshold)
 
-        # cross_talk_eval = CrossTalkEvaluator('name of the model')
-        #
-        # # seq1, seq2 can be either strings or numpy arrays (I accept both and have tested it)
-        # bind_values_1, bind_values_2 = cross_talk_eval.run(seq1,seq2)
-        # figure = plot_bindings(threshold, bind_values_1, bind_values_2)
+        cross_talk_eval = CrossTalkEvaluator('name of the model')
+
+        # seq1, seq2 can be either strings or numpy arrays (I accept both and have tested it)
+        bind_values_1, bind_values_2 = cross_talk_eval.run(seq1,seq2)
+        figure = plot_bindings(threshold, bind_values_1, bind_values_2)
 
         layout = QVBoxLayout()
         label = QLabel("Threshold")
@@ -123,7 +129,18 @@ class RMW(QtWidgets.QWidget):
             label = QtWidgets.QLabel("Supported file types: .csv, .txt, and .npy \
                                                                           \nGo to [url for github docs] for example file formats.");
 
-            filepath = FileDialog()
+            filepath_dialog = FileDialog()
+            filepath_dialog.directory = os.path.join(filepath_dialog.directory, 'data')
+            filepath_dialog.filter = "Text, CSV files (*.txt; *.csv);; Text files (*.txt);; CSV files (*.csv)"
+            npy_filepaths_dialog = MultiFileDialog()
+            npy_filepaths_dialog.directory = os.path.join(npy_filepaths_dialog.directory, 'data')
+            npy_filepaths_dialog.filter = "Numpy files (*.npy)"
+            npy_filepaths_dialog.setVisible(False)
+
+            csv_txt_btn.toggled.connect(lambda: filepath_dialog.setVisible(True))
+            csv_txt_btn.toggled.connect(lambda: npy_filepaths_dialog.setVisible(False))
+            npy_btn.toggled.connect(lambda: filepath_dialog.setVisible(False))
+            npy_btn.toggled.connect(lambda: npy_filepaths_dialog.setVisible(True))
 
             cancel = QtWidgets.QPushButton("Cancel")
             cancel.clicked.connect(dialog.close)
@@ -135,7 +152,8 @@ class RMW(QtWidgets.QWidget):
             button_layout.addWidget(ok)
             layout = QtWidgets.QVBoxLayout()
             layout.addWidget(label)
-            layout.addWidget(filepath)
+            layout.addWidget(filepath_dialog)
+            layout.addWidget(npy_filepaths_dialog)
             layout.addLayout(button_layout)
             dialog.setLayout(layout)
             dialog.exec_()
